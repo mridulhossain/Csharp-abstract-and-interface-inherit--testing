fe1581053298c9dbb15e0717840a826b33080790
mridulhossain <54486136+mridulhossain@users.noreply.github.com>
2021-09-03 03:07:42 +0600

    Update Program.cs

--- OOP/Program.cs
+++ OOP/Program.cs
@@ -8,6 +8,8 @@ namespace OOP
         static void Main(string[] args)
         {
             // you can inherit abstract to abstract and also implement interface in abstract class.
+            // you can't inherit a normal class
+            
             DataAccess a = new Information();
             Console.WriteLine(a.Count());
             a.Connect();
